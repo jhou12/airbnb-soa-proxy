8cff4202135724917d20b0c9e85aab9cc1a4804b
Jenny Hou <jhou2012@gmail.com>
2021-03-29 17:09:52 -0400

    update service routes and bundles

--- server/index.js
+++ server/index.js
@@ -1,29 +1,19 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const axios = require('axios')
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const cors = require('cors')
 const path = require('path')
 
 app.use(cors())
 
-// DANE
-app.use('/api/headerService/:propertyId', createProxyMiddleware({ target: 'http://54.211.95.226:5001', changeOrigin: true }))
+// REVIEWS SERVICE
+// app.use('/reviews/propId/:id', createProxyMiddleware({ target: 'http://localhost:3001', changeOrigin: true }))
+app.use('/reviews/propId/:id', createProxyMiddleware({ target: 'http://3.143.168.52:3001', changeOrigin: true }))
 
-app.use('/api/hostedByService/:propertyId', createProxyMiddleware({ target: 'http://54.237.132.122:5002', changeOrigin: true }))
-
-// PAULY
-app.use('/propertyDetails/:pId', createProxyMiddleware({ target: 'http://13.56.218.102:5545', changeOrigin: true }))
-
-app.use('/map/:pId', createProxyMiddleware({ target: 'http://54.215.197.139:4454/', changeOrigin: true }))
-
-// JENNY
-// app.use('/reviews/propId/:id', createProxyMiddleware({ target: 'http://localhost:1984', changeOrigin: true }))
-app.use('/reviews/propId/:id', createProxyMiddleware({ target: 'http://52.15.83.247:1984', changeOrigin: true }))
-
-// app.use('/morePlaces/propId/:id', createProxyMiddleware({ target: 'http://localhost:1985', changeOrigin: true }))
-app.use('/morePlaces/propId/:id', createProxyMiddleware({ target: 'http://3.18.223.111:1985', changeOrigin: true }))
+// MORE PLACES SERVICE
+// app.use('/morePlaces/propId/:id', createProxyMiddleware({ target: 'http://localhost:3002', changeOrigin: true }))
+app.use('/morePlaces/propId/:id', createProxyMiddleware({ target: 'http://18.223.160.190:3002', changeOrigin: true }))
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
